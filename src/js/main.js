1bc794e5041f381b93600be032c1669406b94dad
fillika <49833052+fillika@users.noreply.github.com>
2019-07-16 18:37:53 +0300

    Работаю над формой регистрации
    Нужно закончить checkbox "Получать спецпредложения"

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,5 @@
 console.log('First file');
 
 $(document).ready(function(){ // Для проверки работоспособности jQuery
-    $('.wrapper').click( function() {
-        console.log('Click')
-    })
+  
 });
